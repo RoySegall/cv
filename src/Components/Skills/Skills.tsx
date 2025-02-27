9826ab6727c0350181425fd20fec5d81e3f22217
Roy Segall <roy@segall.io>
2025-02-27 17:23:41 +0200

    mor work on strucutre

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,8 +1,30 @@
 import "./skills.css";
 import {BlockTitle} from "../BlockTitle";
+import type {ReactNode} from "react";
+
+function Block({title, children}: {title: string, children: ReactNode}) {
+    return <div id='block'>
+        <BlockTitle>{title}</BlockTitle>
+        <p>
+            {children}
+        </p>
+    </div>
+}
 
 export function Skills() {
     return <div id='skills'>
-        <BlockTitle>Skills</BlockTitle>
+
+        <Block title={'Languages'}>
+            asasd
+        </Block>
+
+        <Block title={'Skills'}>
+            asasd
+        </Block>
+
+        <Block title={'VOLUNTEERING'}>
+            asasd
+        </Block>
+
     </div>
 }
