663da55c988ab424f008166f85e3bb3834e3d4e8
Roy Segall <roy@segall.io>
2025-02-27 17:44:36 +0200

    Dont with languages

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -11,11 +11,27 @@ function Block({title, children}: {title: string, children: ReactNode}) {
     </div>
 }
 
+function Language({language, level, rate}: {language: string, level: string, rate: number}) {
+    return <div id='language'>
+        <div id='description'>
+            <span className='name'>{language}</span>
+            <span className='level'>{level}</span>
+        </div>
+
+        <ul>
+            {Array.from({length: 5}).map((_, i) => <li key={i} className={i + 1 > rate ? 'off' : ''}>&nbsp;</li>)}
+        </ul>
+
+    </div>
+}
+
 export function Skills() {
     return <div id='skills'>
 
         <Block title={'Languages'}>
-            asasd
+            <Language language='Hebrew' level='Native' rate={5} />
+            <span id='spacer'></span>
+            <Language language='English' level='Proficient' rate={4} />
         </Block>
 
         <Block title={'Skills'}>
