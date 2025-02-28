abae6b3b35742a9377caf1b2b18352c281bf7eec
Roy Segall <roy@segall.io>
2025-02-28 16:42:45 +0200

    Remove the file before generating anerw one

--- scripts/generatePdf.ts
+++ scripts/generatePdf.ts
@@ -1,16 +1,21 @@
 import puppeteer, {Browser} from "puppeteer";
 import {resolve} from "path";
+import {unlink} from "node:fs/promises";
 
 let browser: Browser;
 async function generatePdf() {
     console.log('starting to open the browser');
+    const filePath = resolve(process.cwd(), 'output', 'cv.pdf');
+
+    await unlink(filePath);
+
     browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto('http://localhost:5173/',  { waitUntil: 'networkidle2' });
 
     await page.pdf({
-        path: resolve(process.cwd(), 'output', 'cv.pdf'),
+        path: filePath,
         format: 'A4',
         printBackground: true,
         margin: { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' } // שוליים מותאמים
