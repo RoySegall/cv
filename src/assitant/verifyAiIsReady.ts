e4be0ffbb42450fab5c0840fd78eecb481c0b648
Roy Segall <roy@segall.io>
2025-03-13 19:34:56 +0200

    Validating the model exists before using it

--- src/assitant/verifyAiIsReady.ts
+++ src/assitant/verifyAiIsReady.ts
@@ -1,3 +1,17 @@
+import ollama from "ollama";
+
 export async function verifyAiIsReady() {
-    console.log('Verifying we can interact with ollama');
+    try {
+        const list = await ollama.list();
+
+        const model = 'granite3.2-vision';
+        const modelExists = list.models.find(({name}) => name.includes(model))
+
+        if (!modelExists) {
+            console.log(`We could not find the ${model} model. Pulling it`);
+            await ollama.pull({model})
+        }
+    } catch {
+        throw new Error('Seems we cannot get list of models from Ollama. Please make sure the server is running and the models are available');
+    }
 }
